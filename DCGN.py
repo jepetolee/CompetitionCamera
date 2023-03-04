eddfd61473ab95ccc016e495572745aacc1b6c84
jepetolee <55366212+jepetolee@users.noreply.github.com>
2023-03-04 23:20:31 +0900

    Minimized the Model

--- DCGN.py
+++ DCGN.py
@@ -7,8 +7,37 @@ import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 
-from MOE import MoE
 
+class WisePooling(Module):
+
+    def __init__(self):
+        super(WisePooling, self).__init__()
+
+    def forward(self, input, graph):
+        tensor_list = list()
+        for j in range(graph.shape[0]):
+            shot_boundary = graph[j]
+            tensor_list.append(
+                torch.div(torch.sum(input[shot_boundary[0]:shot_boundary[1] + 1], dim=0).requires_grad_(True),
+                          shot_boundary[1] - shot_boundary[0] + 1).requires_grad_(True) + 6e-3)
+        return torch.stack(tensor_list, dim=0).requires_grad_(True)
+
+
+class WiseConvolution(Module):
+
+    def __init__(self, input_size, output_size):
+        super(WiseConvolution, self).__init__()
+        self.WiseConv = nn.Linear(input_size, output_size)
+
+    def forward(self, input, graph):
+        tensor_list = list()
+        for j in range(graph.shape[0]):
+            shot_boundary = graph[j]
+
+            tensor_list.append(
+                torch.sum(self.WiseConv(input[shot_boundary[0]:shot_boundary[1] + 1]), dim=0).requires_grad_(True))
+
+        return torch.stack(tensor_list, dim=0).requires_grad_(True)
 
 
 class NodeConvolution(Module):
@@ -24,54 +53,30 @@ class NodeConvolution(Module):
         self.weight1.data.uniform_(-stdv, stdv)
 
     def forward(self, input):
-        batch_size = input.shape[0]
-        steps = batch_size // self.pooling_size
-        left_tensor = False
-        if batch_size % self.pooling_size != 0:
-            steps += 1
-            left_tensor = True
+        batch_list = list()
+        for i in range(input.shape[0]):
+            tensor = input[i]
+            batch_size = tensor.shape[0]
+            steps = batch_size // self.pooling_size
+            left_tensor = False
+            if batch_size % self.pooling_size != 0:
+                steps += 1
+                left_tensor = True
+            tensor_list = list()
+            for j in range(steps):
+                if left_tensor is True and j == steps - 1:
+                    tensor_ = torch.zeros(self.pooling_size, tensor.shape[1], requires_grad=True).cuda()
+                    for i in range(batch_size % self.pooling_size):
+                        tensor_[i] = tensor[self.pooling_size * j + i]
+                    tensor_list.append(torch.sum(tensor_ * self.weight1, dim=0).requires_grad_(True))
+                else:
+                    tensor_list.append(
+                        torch.sum(
+                            tensor[self.pooling_size * j + 0:self.pooling_size * j + self.pooling_size] * self.weight1,
+                            dim=0).requires_grad_(True))
+            batch_list.append(torch.stack(tensor_list, dim=0))
 
-        tensor_list = list()
-        for j in range(steps):
-            if left_tensor is True and j == steps - 1:
-                tensor=torch.zeros(self.pooling_size,input.shape[1]).cuda()
-                for i in range(batch_size % self.pooling_size):
-                    tensor[i]=input[self.pooling_size * j + i]
-                tensor_list.append(torch.sum(tensor * self.weight1,dim=0))
-            else:
-                tensor_list.append(torch.sum(input[self.pooling_size * j + 0:self.pooling_size * j + self.pooling_size] * self.weight1,dim=0))
-
-
-        return torch.stack(tensor_list, dim=0)
-
-
-class WisePooling(Module):
-
-    def __init__(self):
-        super(WisePooling, self).__init__()
-
-    def forward(self, input, graph):
-        tensor_list =list()
-        for j in range(graph.shape[0]):
-            shot_boundary = graph[j]
-            tensor_list.append(torch.div(torch.sum(input[shot_boundary[0]:shot_boundary[1] + 1], dim=0),shot_boundary[1] - shot_boundary[0] + 1)+6e-3)
-        return torch.stack(tensor_list, dim=0)
-
-
-class WiseConvolution(Module):
-
-    def __init__(self, input_size, output_size):
-        super(WiseConvolution, self).__init__()
-        self.WiseConv = nn.Linear(input_size, output_size)
-
-    def forward(self, input, graph):
-        tensor_list = list()
-        for j in range(graph.shape[0]):
-            shot_boundary = graph[j]
-
-            tensor_list.append(torch.sum(self.WiseConv(input[shot_boundary[0]:shot_boundary[1] + 1]), dim=0))
-
-        return torch.stack(tensor_list, dim=0)
+        return torch.stack(batch_list, dim=0).requires_grad_(True)
 
 
 class GraphAttentionPooling(Module):
@@ -82,87 +87,112 @@ class GraphAttentionPooling(Module):
         self.W = nn.Linear(in_features, 1, bias=True)
         self.pooling_size = pooling_size
 
-    def forward(self, batch_rep):
-        batch_size = batch_rep.shape[0]
-        steps = batch_size // self.pooling_size
-        left_tensor = False
-        if batch_size % self.pooling_size != 0:
-            steps += 1
-            left_tensor = True
+    def forward(self, batch_tensor):
+        batch_list = list()
+        for i in range(batch_tensor.shape[0]):
+            tensor = batch_tensor[i]
+            batch_size = tensor.shape[0]
+            steps = batch_size // self.pooling_size
+            left_tensor = False
+            if batch_size % self.pooling_size != 0:
+                steps += 1
+                left_tensor = True
 
-        tensor_list = list()
-        for j in range(steps):
-            if left_tensor is True and j == steps - 1:
-                left_size = self.pooling_size - batch_size % self.pooling_size
+            tensor_list = list()
+            for j in range(steps):
+                if left_tensor is True and j == steps - 1:
+                    tensor_ = torch.zeros(self.pooling_size, tensor.shape[1], requires_grad=True).cuda()
+                    for i in range(batch_size % self.pooling_size):
+                        tensor_[i] = tensor[self.pooling_size * j + i]
+                    att_w = F.softmax(self.W(tensor_), dim=0).requires_grad_(True)
+                    tensor_list.append(tensor_.T @ att_w)
+                else:
+                    att_w = F.softmax(
+                        self.W(tensor[self.pooling_size * j + 0:self.pooling_size * j + self.pooling_size]),
+                        dim=0).requires_grad_(True)
+                    tensor_list.append(
+                        tensor[self.pooling_size * j + 0:self.pooling_size * j + self.pooling_size].T @ att_w)
+            batch_list.append(torch.stack(tensor_list, dim=0))
+        return torch.stack(batch_list, dim=0).requires_grad_(True)
 
-                att_w = F.softmax(self.W(
-                    batch_rep[self.pooling_size * j + 0 - left_size: self.pooling_size * j + self.pooling_size - left_size]),dim=0)
-                tensor_list.append(batch_rep[self.pooling_size * j + 0 - left_size: self.pooling_size * j + self.pooling_size - left_size].T @ att_w)
-            else:
-                att_w = F.softmax(
-                    self.W(batch_rep[self.pooling_size * j + 0:self.pooling_size * j + self.pooling_size]), dim=0)
-                tensor_list.append(batch_rep[self.pooling_size * j + 0:self.pooling_size * j + self.pooling_size].T @ att_w)
 
-        return torch.stack(tensor_list, dim=0)
+# v1 1024 256 64 16   4               91 78
+# v2 1024 512 128 32  4              78 75
+# v3 1024 512 256 128 8
+
+class FinalModel(nn.Module):
+    def __init__(self):
+        super(FinalModel, self).__init__()
+        self.feature_extract = nn.Sequential(
+            nn.Conv2d(3, 5, kernel_size=3),
+            nn.GELU(),
+            nn.BatchNorm2d(5),
+            nn.MaxPool2d(3),
+            nn.Dropout(0.6),
+            nn.Conv2d(5, 8, kernel_size=3),
+            nn.GELU(),
+            nn.BatchNorm2d(8),
+            nn.MaxPool2d(3),
+            nn.Dropout(0.6))
+        self.dcgn = DCGN(5832, 13)
+
+    def forward(self, input, device):
+        batch_size = input.shape[0]
+        arrays = list()
+        for i in range(batch_size):
+            arrays.append(self.feature_extract(input[i]))
+
+        x = torch.stack(arrays, dim=0)
+        x = x.view(batch_size, 50, -1)
+        return self.dcgn(x, device)
 
 
 class DCGN(nn.Module):
     def __init__(self, input, nclass, pooling_size=3):
         super(DCGN, self).__init__()
 
-        self.nodewiseconvolution = NodeConvolution(3, input,pooling_size=pooling_size)
+        self.nodewiseconvolution = NodeConvolution(pooling_size, input, pooling_size=pooling_size)
         self.WisePooling = GraphAttentionPooling(input, pooling_size=pooling_size)
-        self.Propagate1 = Parameter(torch.FloatTensor(1024, 1024))
+        self.Propagate1 = Parameter(torch.FloatTensor(input, 216))
 
-        self.NodeConvolution1 = NodeConvolution(3, 1024,pooling_size=pooling_size)
-        self.AttentionPooling1 = GraphAttentionPooling(1024, pooling_size=pooling_size)
-        self.Propagate2 = Parameter(torch.FloatTensor(1024, 512))
-
-        self.NodeConvolution2 = NodeConvolution(3, 512, pooling_size=pooling_size)
-        self.AttentionPooling2 = GraphAttentionPooling(512, pooling_size=pooling_size)
-        self.Propagate3 = Parameter(torch.FloatTensor(512, 256))
+        self.NodeConvolution2 = NodeConvolution(pooling_size, 216, pooling_size=pooling_size)
+        self.AttentionPooling2 = GraphAttentionPooling(216, pooling_size=pooling_size)
+        self.Propagate3 = Parameter(torch.FloatTensor(216, 8))
 
         self.reset_parameters()
-        self.MixtureOfExpert = MoE(256*2, nclass, nclass, hidden_size=64, noisy_gating=True, k=4)
+        self.classifier = nn.Linear(8 * 6, nclass)
 
     def reset_parameters(self):
         stdv = 1. / math.sqrt(self.Propagate1.size(1))
         self.Propagate1.data.uniform_(-stdv, stdv)
 
-        stdv = 1. / math.sqrt(self.Propagate2.size(1))
-        self.Propagate2.data.uniform_(-stdv, stdv)
-
         stdv = 1. / math.sqrt(self.Propagate3.size(1))
         self.Propagate3.data.uniform_(-stdv, stdv)
 
-    def forward(self, x, graph, device):
+    def forward(self, x, device):
 
         adj = self.WisePooling(x)
         x = self.nodewiseconvolution(x)  # 2,256
-          # 2,1024
-        adj = self.get_adjacent(adj).to(device)  # 2,2
-
-        x = adj@x@ self.Propagate1
-        x = F.elu(x)
-
-        adj = self.AttentionPooling1(x)  # 2,64
-        x = self.NodeConvolution1(x)  # 2,64
-        adj = self.get_adjacent(adj).to(device)  # 2,32.
-
-        x = adj @ x @ self.Propagate2
-        x = F.elu(x)
+        adj = self.get_adjacent(adj).to(device).requires_grad_(True)  # 2,2
+        x = self.propagate(adj, x, self.Propagate1)
+        x = F.gelu(x)
 
         adj = self.AttentionPooling2(x)  # 2,64
         x = self.NodeConvolution2(x)  # 2,64
-        adj = self.get_adjacent(adj).to(device)  # 2,32.
+        adj = self.get_adjacent(adj).to(device).requires_grad_(True)  # 2,32.
+        x = self.propagate(adj, x, self.Propagate3)
+        x = F.gelu(x)
 
-        x = adj @ x @ self.Propagate3
-        x = F.elu(x)
-
-        x= x.view(-1,512)
-        x = self.MixtureOfExpert(x)
+        x = x.view(-1, 6*8)
+        x = self.classifier(x)
 
         return x
+
+    def propagate(self, adj, x, propagate):
+        batch_list = list()
+        for i in range(x.shape[0]):
+            batch_list.append(adj[i] @ x[i] @ propagate)
+        return torch.stack(batch_list, dim=0).requires_grad_(True)
 
     def cosine_similarity_adjacent(self, matrix1, matrix2):
         squaresum1 = torch.sum(torch.square(matrix1), dim=1)  # 1024 to 1
@@ -177,19 +207,23 @@ class DCGN(nn.Module):
         return cosine_similarity
 
     def get_adjacent(self, matrix):
-        matrix_frame = matrix.shape[0]  # 4,2,1024
-        AdjacentMatrix = torch.zeros( matrix_frame, matrix_frame)  # 2 X 2
+        batch_list = list()
+        for i in range(matrix.shape[0]):
+            tensor = matrix[i]
+            matrix_frame = tensor.shape[0]  # 4,2,1024
+            AdjacentMatrix = torch.zeros(matrix_frame, matrix_frame)  # 2 X 2
 
-        chunks = torch.chunk(matrix, matrix_frame, dim=0)
-        for i in range(matrix_frame):
-            for j in range(matrix_frame - i):
-                AdjacentMatrix[j][i] = self.cosine_similarity_adjacent(chunks[i], chunks[j])
-                if not i == j:
-                    AdjacentMatrix[j][i] = AdjacentMatrix[i][j]
-        I = torch.eye(AdjacentMatrix.shape[0])
+            chunks = torch.chunk(tensor, matrix_frame, dim=0)
+            for i in range(matrix_frame):
+                for j in range(matrix_frame - i):
+                    AdjacentMatrix[j][i] = self.cosine_similarity_adjacent(chunks[i], chunks[j])
+                    if not i == j:
+                        AdjacentMatrix[j][i] = AdjacentMatrix[i][j]
+            I = torch.eye(AdjacentMatrix.shape[0], requires_grad=True)
 
-        AdjacentMatrix += I
-        D_hat = torch.sum(AdjacentMatrix, dim=0)
-        D_hat = torch.linalg.inv(torch.sqrt(torch.diag(D_hat)))
-
-        return D_hat @ AdjacentMatrix @ D_hat
+            AdjacentMatrix += I
+            AdjacentMatrix = AdjacentMatrix.requires_grad_(True)
+            D_hat = torch.sum(AdjacentMatrix, dim=0)
+            D_hat = torch.linalg.inv(torch.sqrt(torch.diag(D_hat)))
+            batch_list.append(D_hat @ AdjacentMatrix @ D_hat)
+        return torch.stack(batch_list, dim=0).requires_grad_(True)
--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 import DCGN
 import h5py
 import warnings
-from tqdm import trange
 import random
 import os
 import cv2
@@ -13,58 +12,96 @@ from tqdm.auto import tqdm
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import f1_score
+import albumentations as A
+from skimage.util import random_noise
 
 
-def train_model(model, optimizer, train_loader, val_loader, scheduler, device):
-
+def inference(model, test_loader, device):
     model.to(device)
-    criterion = nn.CrossEntropyLoss().to(device)
-
-    best_val_score = 0
-    best_model = None
-
-    for epoch in range(1, CFG['EPOCHS'] + 1):
-        model.train()
-        train_loss = []
-        for videos,graph, labels in tqdm(iter(train_loader)):
+    model.eval()
+    preds = []
+    with torch.no_grad():
+        for videos in tqdm(iter(test_loader)):
             videos = videos.to(device)
-            labels = labels.to(device)
 
-            optimizer.zero_grad()
-            output, aux_loss = model(videos.reshape(50,1024),graph[0], device)
-            loss = criterion(output, labels)
-            loss += aux_loss
+            logit = model(videos, device)
 
-            loss.backward()
-            optimizer.step()
-            train_loss.append(loss.item())
+            preds += logit.argmax(1).detach().cpu().numpy().tolist()
+    return preds
 
-        _train_loss = np.mean(train_loss)
-        _val_loss, _val_score = validation(model, criterion, val_loader, device)
-        print(
-            f'Epoch [{epoch}], Train Loss : [{_train_loss:.5f}] Val Loss : [{_val_loss:.5f}] Val F1 : [{_val_score:.5f}]')
 
-        if scheduler is not None:
-            scheduler.step(_val_score)
+class CustomDataset(Dataset):
+    def __init__(self, video_path_list, label_list, test=True):
+        self.test = test
+        self.original = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']), A.Normalize()], p=1)
+        self.flip = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']),
+                               A.HorizontalFlip(p=1), A.Normalize()], p=1)
+        self.bright = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']),
+                                 A.RandomBrightness(limit=(-0.8, 0.8), p=1), A.Normalize()], p=1)
+        self.blur = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']),
+                               A.ElasticTransform(p=1), A.Normalize()], p=1)
+        self.noise = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']),
+                               A.GaussNoise(p=1), A.Normalize()], p=1)
+        self.distort = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']),
+                               A.GridDistortion(p=1), A.Normalize()], p=1)
+        self.dropout = A.Compose([A.Resize(width=CFG['IMG_SIZE'], height=CFG['IMG_SIZE']),
+                               A.CoarseDropout(p=1,max_holes=7,max_width=8,max_height=8,min_holes=3), A.Normalize()], p=1)
+        self.video_path_list = video_path_list
+        self.label_list = label_list
 
-        if best_val_score < _val_score:
-            best_val_score = _val_score
-            best_model = model
-    return best_model
+    def __getitem__(self, index):
+        frames = self.get_video(self.video_path_list[index])
+
+        if self.label_list is not None:
+            label = self.label_list[index]
+
+            return frames, label
+        else:
+            return frames
+
+    def __len__(self):
+        return len(self.video_path_list)
+
+    def get_video(self, path):
+        with torch.no_grad():
+            frames = []
+            cap = cv2.VideoCapture(path)
+            seed = random.randint(1, 100)
+            if self.test is True:
+                func = self.original
+            elif 1 <= seed <= 30:
+                func = self.original
+            elif 31 <= seed <= 50:
+                func = self.flip
+            elif 51 <= seed <= 60:
+                func = self.blur
+            elif 61 <= seed <= 70:
+                func = self.bright
+            elif 71 <= seed <= 80:
+                func = self.distort
+            elif 81<= seed <= 90:
+                func = self.dropout
+            else:
+                func = self.noise
+
+            for i in range(CFG['VIDEO_LENGTH']):
+                _, img = cap.read()
+                img = func(image=np.asarray(img))['image']
+                img = np.rollaxis(img, 2)
+                frames.append(torch.from_numpy(img))
+        return torch.stack(frames, dim=0).float()
 
 
 def validation(model, criterion, val_loader, device):
     model.eval()
     val_loss = []
     preds, trues = [], []
-
     with torch.no_grad():
-        for videos,graph, labels in tqdm(iter(val_loader)):
+        for videos, labels in tqdm(iter(val_loader)):
             videos = videos.to(device)
+            logit = model(videos, device)
             labels = labels.to(device)
-            logit, aux_loss  = model(videos.reshape(50,1024),graph[0],device)
-
-            loss = criterion(logit, labels) +aux_loss
+            loss = criterion(logit, labels)
 
             val_loss.append(loss.item())
 
@@ -77,109 +114,62 @@ def validation(model, criterion, val_loader, device):
     return _val_loss, _val_score
 
 
+def train_model(model, optimizer, device, valid_mod=True):
+    model.to(device)
+    criterion = nn.CrossEntropyLoss().to(device)
 
-def seed_everything(seed):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    best_score = 0
+    best_model = None
+    preds, trues = [], []
+    df = pd.read_csv('./train.csv')
 
+    for epoch in range(1, CFG['EPOCHS'] + 1):
+        train, val, _, _ = train_test_split(df, df['label'], test_size=0.2)
 
+        train_dataset = CustomDataset(train['video_path'].values, train['label'].values, test=False)
+        train_loader = DataLoader(train_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=True, num_workers=0)
 
-def dhash(img):
-    img = cv2.resize(img, (9, 8), interpolation=cv2.INTER_CUBIC)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        val_dataset = CustomDataset(val['video_path'].values, val['label'].values, test=True)
+        val_loader = DataLoader(val_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=False, num_workers=0)
+        model.train()
 
-    dhash_str = ''
-    for i in range(8):
-        for j in range(8):
-            if gray[i, j] > gray[i, j + 1]:
-                dhash_str = dhash_str + '1'
-            else:
-                dhash_str = dhash_str + '0'
-    result = ''
-    for i in range(0, len(dhash_str), 4):
-        result += ''.join('%x' % int(dhash_str[i: i + 4], 2))
-    return result
+        train_loss = []
+        for videos, labels in tqdm(iter(train_loader)):
+            videos = videos.to(device)
 
+            optimizer.zero_grad()
+            output = model(videos, device)
+            labels = labels.to(device)
+            loss = criterion(output, labels)
 
-def hanming(s1, s2):
-    assert len(s1) == len(s2)
-    return sum([ch1 != ch2 for ch1, ch2 in zip(s1, s2)])
+            loss.backward()
+            optimizer.step()
+            train_loss.append(loss.item())
 
-import torchvision.models as models
-from KTS import kernel, cpd_auto2
-class CustomDataset(Dataset):
-    def __init__(self, video_path_list, label_list,saved=True,test=True):
+            pred = output.argmax(1)
 
-        self.video_path_list = video_path_list
-        self.label_list = label_list
-        self.saved =saved
+            preds += pred.detach().cpu().numpy().tolist()
+            trues += labels.detach().cpu().numpy().tolist()
 
-        if saved:
-            if test:
-                url = './my_data/test.h5'
-            else:
-                url = 'G:/CompetitionCamera/my_data/train.h5'
-            self.f1 = h5py.File(url, 'r')
-
-    def __getitem__(self, index):
-        if self.saved:
-            frames, graph = self.get_data(self.video_path_list[index])
+        _train_loss = np.mean(train_loss)
+        train_score = f1_score(trues, preds, average='macro')
+        if valid_mod:
+            _val_loss, _val_score = validation(model, criterion, val_loader, device)
+            print(f'Epoch [{epoch}], Train Loss : [{_train_loss:.5f}], Train F1 : [{train_score:.5f}],Val Loss : [{_val_loss:.5f}] Val F1 : [{_val_score:.5f}]')
+            if best_score < _val_score:
+                best_score = _val_score
+                best_model = model
+                torch.save(best_model.state_dict(), './saved.pt')
         else:
-            frames,graph = self.get_video(self.video_path_list[index])
 
-        if self.label_list is not None:
-            label = self.label_list[index]
-            return frames,graph, label
-        else:
-            return frames,graph
+            print(
+                f'Epoch [{epoch}], Train Loss : [{_train_loss:.5f}] Train F1 : [{train_score:.5f}]')
+            if best_score < train_score:
+                best_score = train_score
+                best_model = model
+                torch.save(best_model.state_dict(), './saved.pt')
 
-    def __len__(self):
-        return len(self.video_path_list)
-
-    def get_data(self,path):
-        path = path[8:]
-        return torch.from_numpy(self.f1[path]['features'][...].astype(np.float32)), self.f1[path]["change_points"][...].astype(np.int32)
-
-    def get_video(self, path):
-
-        googlenet = models.googlenet(pretrained=True)
-        googlenet = torch.nn.Sequential(*list(googlenet.children())[:-2]).cuda()
-        googlenet.eval()
-
-        with torch.no_grad():
-            frames = []
-            cap = cv2.VideoCapture(path)
-            hash1 = None
-            base = None
-            for i in range(CFG['VIDEO_LENGTH']):
-                _, img = cap.read()
-                hash2 = dhash(img)
-                if hash1 is not None:
-                    dist = hanming(hash1, hash2)
-                if base is None or dist > 4:
-                    base = img
-                    hash1 = hash2
-                    tensor = googlenet(torch.from_numpy(np.rollaxis(img, 2)).cuda().float().reshape(-1,3, 720, 1280))
-                    frames.append(torch.squeeze(tensor))
-                else:
-                    tensor = googlenet(torch.from_numpy(np.rollaxis(base, 2)).cuda().float().reshape(-1,3, 720, 1280))
-                    frames.append(torch.squeeze(tensor))
-        tensor = torch.stack(frames,dim=0)
-        K = tensor.cpu().detach().numpy()
-        K1 = kernel(K, K.T, K.shape[0])
-        m = round(50 / 106 * 2)
-        cps1, scores1 = cpd_auto2(K1, m, 1.0, 1)
-        cps1 *= 1
-        cps1 = np.hstack((0, cps1, 50))
-        begin_frames = cps1[:-1]
-        end_frames = cps1[1:]
-        cps1 = np.vstack((begin_frames, end_frames - 1)).T
-        return tensor,cps1
+    return best_model
 
 
 if __name__ == "__main__":
@@ -189,52 +179,24 @@ if __name__ == "__main__":
 
     CFG = {
         'VIDEO_LENGTH': 50,
-        'IMG_SIZE': 128,
-        'EPOCHS': 100,
-        'LEARNING_RATE': 0.005,
-        'BATCH_SIZE': 1,
-        'SEED': 41
+        'EPOCHS': 40,
+        'IMG_SIZE': 256,
+        'LEARNING_RATE': 0.0002,
+        'BATCH_SIZE': 8
     }
-    seed_everything(CFG['SEED'])
-    df = pd.read_csv('./train.csv')
-    train, val, _, _ = train_test_split(df, df['label'], test_size=0.2, random_state=CFG['SEED'])
 
-    train_dataset = CustomDataset(train['video_path'].values, train['label'].values,test=False)
-    train_loader = DataLoader(train_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=True, num_workers=0)
+    state_dict = torch.load('./saved.pt')
+    model = DCGN.FinalModel().to(device)
+    model.load_state_dict(state_dict)
+    optimizer = torch.optim.RAdam(params=model.parameters(), lr=CFG["LEARNING_RATE"], weight_decay=1e-5)
 
-    val_dataset = CustomDataset(val['video_path'].values, val['label'].values,test=False)
-    val_loader = DataLoader(val_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=False, num_workers=0)
-
-    model = DCGN.DCGN(1024, 13)
-    check_point = torch.load('./saved.pt')
-    model.load_state_dict(check_point)
-    model.eval()
-    optimizer = torch.optim.Adam(params=model.parameters(), lr=CFG["LEARNING_RATE"])
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=2,
-                                                           threshold_mode='abs', min_lr=1e-8, verbose=True)
-
-    infer_model = train_model(model, optimizer, train_loader, val_loader, scheduler, device)
+    infer_model = train_model(model, optimizer, device)  # with valid
 
     test = pd.read_csv('./test.csv')
-    test_dataset = CustomDataset(test['video_path'].values,None,test=True)
+    test_dataset = CustomDataset(test['video_path'].values, None, test=True)
     test_loader = DataLoader(test_dataset, batch_size=CFG['BATCH_SIZE'], shuffle=False, num_workers=0)
 
-
-    def inference(model, test_loader, device):
-        model.to(device)
-        model.eval()
-        preds = []
-        with torch.no_grad():
-            for videos in tqdm(iter(test_loader)):
-                videos = videos.to(device)
-
-                logit = model(videos)
-
-                preds += logit.argmax(1).detach().cpu().numpy().tolist()
-        return preds
-
-
-    preds = inference(infer_model, test_loader, device)
+    preds = inference(model, test_loader, device)
 
     submit = pd.read_csv('./sample_submission.csv')
 
